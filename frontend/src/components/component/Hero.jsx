b0de43396791ef818c5ebf29af7c39b0ccee4808
Suraj Maurya <samratsuraj87@gmail.com>
2024-11-30 16:59:54 +0530

    Update Hero.jsx

--- frontend/src/components/component/Hero.jsx
+++ frontend/src/components/component/Hero.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Hero = () => {
     const navigate = useNavigate()
